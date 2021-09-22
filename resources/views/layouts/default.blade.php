576dab1e1876855f42cbad01dfcbfb44bb76d08c
Hairul Jr <januarhairul@gmail.com>
2021-09-22 16:03:14 +0700

    Style, WIP : Add Ela Admin Template

---
+++ resources/views/layouts/default.blade.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html class="no-js" lang="">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>@yield('title')</title>
+    <meta name="description" content="Ela Admin - HTML5 Admin Template">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    @include('includes.style')
+    @stack('addon-style')
+</head>
+
+<body>
+    <!-- Left Panel -->
+    @include('includes.sidebar')
+    <!-- /#left-panel -->
+
+    <!-- Right Panel -->
+    <div id="right-panel" class="right-panel">
+        <!-- Header-->
+        @include('includes.navbar')
+        <!-- /#header -->
+
+        <!-- Content -->
+        @yield('content')
+        <!-- /.content -->
+
+        <div class="clearfix"></div>
+        
+        <!-- Footer -->
+        @include('includes.footer')
+        <!-- /.site-footer -->
+    </div>
+    <!-- /#right-panel -->
+
+    <!-- Scripts -->
+    @include('includes.script')
+</body>
+</html>
